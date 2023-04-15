23093593a2c4ea8116f0bf956ddc272daa7558c5
Trung <trung180996@gmail.com>
2023-04-15 14:57:53 +0700

    final

--- main.js
+++ main.js
@@ -52,7 +52,10 @@ document.getElementById("btnTotalNumber").onclick = function () {
     //Xử lý
     var totalNumber = number1 + number2 + number3 + number4 + number5;
     var tbTotalNumber = totalNumber / 5;
-    var resultTbNumber = "<p class='alert alert-success'> Giá trị trung bình là: "+ tbTotalNumber +"</p>"
+    var resultTbNumber =
+        "<p class='alert alert-success'> Giá trị trung bình là: " +
+        tbTotalNumber +
+        "</p>";
     // Show
     document.getElementById("resultTotalNumber").innerHTML = resultTbNumber;
 };
@@ -70,14 +73,73 @@ Số tiền USD đổi quy đổi sang VND = Số USD muốn đổi * Tỉ giá 
 Kết quả USD đổi quy đổi sang VND 
  */
 
-document.getElementById("btnChangeMoney").onclick = function() {
+document.getElementById("btnChangeMoney").onclick = function () {
     var USD = 23500;
     var usdMoney = document.getElementById("usdMoney").value * 1;
     // FormatUSD
     var formatUSD = new Intl.NumberFormat("vn-Vn");
     // xử lý
     var resultUsd = USD * usdMoney;
-    var showUsd = "<p class='alert alert-success'> Số tiền được quy đổi là: " + formatUSD.format(resultUsd) + " VND" + "</p>";
+    var showUsd =
+        "<p class='alert alert-success'> Số tiền được quy đổi là: " +
+        formatUSD.format(resultUsd) +
+        " VND" +
+        "</p>";
     // Show
     document.getElementById("resultChangeMoney").innerHTML = showUsd;
-}
+};
+
+/**
+Mô hình 3 khối tính chu vi, diện tích
+*.* Đầu vô
+Chiều dài
+Chiều rộng
+Chu vi 
+Diện tích
+*.* Xử lý
+Chu vi = (Dài + Rộng) * 2
+Diện tích = Dài * Rộng
+*.* Đầu ra
+Kết quả chu vi
+Kết quả diện tích
+*/
+
+document.getElementById("btnTinhChuNhat").onclick = function () {
+    var chieuDai = document.getElementById("chieuDai").value * 1;
+    var chieuRong = document.getElementById("chieuRong").value * 1;
+    //Xử lý
+    var chuVi = (chieuDai + chieuRong) * 2;
+    var dienTich = chieuDai * chieuRong;
+    var resultChuNhat = "";
+    resultChuNhat +=
+        "<p class='alert alert-success'> Chu vi là: " + chuVi + "</p>";
+    resultChuNhat +=
+        "<p class='alert alert-success'> Diện tích là: " + dienTich + "</p>";
+    // Show
+    document.getElementById("resultTinhChuNhat").innerHTML = resultChuNhat;
+};
+
+/**
+Mô hình 3 khối tính tổng 2 ký số
+*.* Đầu vào
+Số
+Tổng 2 ký số
+*.* Xử lý
+Tổng 2 ký số =  hàng chục + hàng đơn vị
+hàng chục = số /10
+hàng đơn vị = số % 10
+*.* Đầu ra
+Kết quả tổng 2 ký số
+*/
+
+document.getElementById("btnTinhKySo").onclick = function () {
+    var kySo = document.getElementById("kySo").value * 1;
+    // Xử lý
+    var hangChuc = Math.floor(kySo / 10);
+    var hangDonVi = kySo % 10;
+    var resultKySo = hangChuc + hangDonVi;
+   
+    var showTongKySo = "<p> Tổng 2 ký số là: " + resultKySo + "</p>";
+    // Show
+    document.getElementById("resultTinhKySo").innerHTML = showTongKySo;
+};
